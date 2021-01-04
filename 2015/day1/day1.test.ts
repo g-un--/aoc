491c968600a11be9c5cc63c3ea1c30acc2324f81
g-un-- <ginoungureanu.mail@gmail.com>
2021-01-04 20:12:42 +0200

    * 2015/day1/part1

--- 2015/day1/day1.test.ts
+++ 2015/day1/day1.test.ts
@@ -1,3 +1,19 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1+2).toBe(3);
+import { part1 } from './day1'
+import fs from 'fs';
+import path from 'path';
+
+describe("2015/day1/part1", () => {
+
+  test('((', () => {
+    expect(part1("((")).toBe("2");
+  });
+
+  test('(())', () => {
+    expect(part1("(())")).toBe("0");
+  });
+
+  test('input.txt', () => {
+    const input = fs.readFileSync(path.resolve(__dirname, "./input.txt"), "utf-8");
+    expect(part1(input)).toBe("138");
+  });
 });
--- 2015/day1/day1.ts
+++ 2015/day1/day1.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+export function part1(input: string): string  {
+    const floor = [...input].reduce((acc, current) => {
+        if (current === "(") {
+            return acc + 1;
+        } else if(current === ")") {
+            return acc - 1;
+        } else {
+            throw `Invalid character in input: ${current}`;
+        }
+    }, 0);
+
+    return floor.toString();
+}
