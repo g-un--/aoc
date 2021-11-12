b8536cafa2de74f9cebe706fa6b4c4dfcf08b727
g-un-- <ginoungureanu.mail@gmail.com>
2021-11-12 22:10:58 +0200

    Refactor

--- 2018/Utils.cs
+++ 2018/Utils.cs
@@ -2,19 +2,24 @@ namespace _2018;
 
 static class Utils
 {
-    public static Task<string[]> ReadInputLines(string day) =>
-         File.ReadAllLinesAsync(Path.Combine(Environment.CurrentDirectory, "input", $"{day}.txt"));
-
-    public static IEnumerable<T> RepeatMany<T>(this T[] input)
+    public static Task<string[]> ReadInputLines(string day) 
     {
-        var current = 0;
-        while (true)
+        var fileName = $"{day}.txt";
+        var input = Path.Combine(Environment.CurrentDirectory, "input", fileName);
+        return File.ReadAllLinesAsync(input);
+    }
+         
+
+    public static IEnumerable<T> RepeatMany<T>(this IEnumerable<T> input)
+    {
+        var shouldContinue = true;
+        while (shouldContinue) 
         {
-            yield return input[current];
-            current += 1;
-            if (current == input.Length)
+            shouldContinue = false;
+            foreach(var item in input) 
             {
-                current = 0;
+                shouldContinue = true;
+                yield return item;
             }
         }
     }
