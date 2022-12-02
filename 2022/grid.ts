b6dfa8b8a2485bf14548b9a8b546c00e6ffef743
g-un-- <ginoungureanu.mail@gmail.com>
2022-12-02 08:19:29 +0200

    format doc

--- 2022/grid.ts
+++ 2022/grid.ts
@@ -1,12 +1,12 @@
 // https://stackoverflow.com/questions/50114294/create-a-grid-array-in-javascript
 
-export const rotate = (grid: string[][]) => 
+export const rotate = (grid: string[][]) =>
   grid[0].map(
-    (_,y)=>grid.map(
-      (_,x)=>[y,x]
+    (_, y) => grid.map(
+      (_, x) => [y, x]
     )
   ).map(
-    row=>row.map(([x,y])=>grid[y][x])
+    row => row.map(([x, y]) => grid[y][x])
   );
 
-export const format = <T>(grid: string[][]) => grid.map(row=>row.join(" ")).join("\n");
+export const format = <T>(grid: string[][]) => grid.map(row => row.join(" ")).join("\n");
--- 2022/utils.ts
+++ 2022/utils.ts
@@ -6,26 +6,26 @@ export function getInput(dirname: string): string[] {
     return input;
 }
 
-export function *repeat<T>(item: T, count: number) {
-    for(let index=0;index<count;index++) {
+export function* repeat<T>(item: T, count: number) {
+    for (let index = 0; index < count; index++) {
         yield item;
     }
 }
 
-export function *range<T>(start: number, end: number) {
-    for(let index=start;index<end;index++) {
+export function* range<T>(start: number, end: number) {
+    for (let index = start; index < end; index++) {
         yield index;
     }
 }
 
-export function *getPermutations<T>(input: Set<T>): Iterable<T[]> {
-    if(input.size === 0) {
+export function* getPermutations<T>(input: Set<T>): Iterable<T[]> {
+    if (input.size === 0) {
         yield [];
     }
-    for(const item of input) {
+    for (const item of input) {
         const remaining = new Set(input);
         remaining.delete(item);
-        for(const items of getPermutations(remaining)) {
+        for (const items of getPermutations(remaining)) {
             const permutation: T[] = [item];
             permutation.push(...items);
             yield permutation;
