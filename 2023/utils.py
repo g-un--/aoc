15f0682aedbd15631804d22ddf2a3acb8bb3fae2
g-un-- <ginoungureanu.mail@gmail.com>
2023-12-02 10:33:58 +0200

    use splitlines

--- 2023/utils.py
+++ 2023/utils.py
@@ -3,8 +3,4 @@ import os
 def getLines(path):
     fileDir = os.path.dirname(os.path.abspath(path))
     inputFile = os.path.join(fileDir, "input.txt")
-    lines = []
-    with open(inputFile, "r") as file:
-        file_iterator = iter(file)
-        lines = list(file_iterator)
-    return lines
+    return open(inputFile, "r").read().splitlines()
