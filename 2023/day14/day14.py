050a5f1b8870e0ff1128f90c203882556bb4fa8a
g-un-- <ginoungureanu.mail@gmail.com>
2023-12-14 15:45:21 +0200

    2023 day 14

--- 2023/day14/day14.py
+++ 2023/day14/day14.py
@@ -4,7 +4,7 @@ import numpy as np
 def getLine(line):
     return [char for char in line]
 
-def moveRocks(array, column):
+def moveRocksNorth(array, column):
     rows, _ = np.shape(array)
     for row in range(0, rows):
         if array[row, column] == '.' or array[row, column] == '#':
@@ -15,13 +15,59 @@ def moveRocks(array, column):
         if k > 1:
             array[row-k+1, column] = array[row, column]
             array[row, column] = '.'
+            
+def moveRocksNorthForAllColumns(array):
+    _, columns = np.shape(array)
+    for column in range(0, columns):
+        moveRocksNorth(array, column)
+        
+def cycle(array):
+    moveRocksNorthForAllColumns(array)
+    array = np.rot90(array, axes=(1, 0))
+    moveRocksNorthForAllColumns(array)
+    array = np.rot90(array, axes=(1, 0))
+    moveRocksNorthForAllColumns(array)
+    array = np.rot90(array, axes=(1, 0))
+    moveRocksNorthForAllColumns(array)
+    array = np.rot90(array, axes=(1, 0))
         
 def part1():
     lines = getLines(__file__)
     array = np.array(list(map(getLine, lines)))
-    rows, columns = np.shape(array)
-    for column in range(0, columns):
-        moveRocks(array, column)
+    rows, _ = np.shape(array)
+    moveRocksNorthForAllColumns(array)
+    sum = 0
+    for row in range(0, rows):
+        count = np.sum(array[row] == 'O')
+        sum += (rows - row) * count
+    return sum
+
+def part2():
+    lines = getLines(__file__)
+    array = np.array(list(map(getLine, lines)))
+    rows, _ = np.shape(array)
+    
+    arrays = []
+    cycles = 0
+    indexOfFound = -1
+    while indexOfFound == -1:
+        cycle(array)
+        cycles += 1
+        
+        for foundIndex, foundArray in enumerate(arrays):
+            if np.array_equal(array, foundArray):
+                indexOfFound = foundIndex
+                break
+        
+        arrays.append(np.copy(array))
+        
+    cycleLength = cycles - indexOfFound - 1
+    remainder = (1000000000 - len(arrays)) % cycleLength
+    array = arrays[-1]
+
+    for _ in range(0, remainder):
+        cycle(array)
+    
     sum = 0
     for row in range(0, rows):
         count = np.sum(array[row] == 'O')
@@ -30,3 +76,6 @@ def part1():
 
 def test_part_1():
     assert part1() == 109345
+    
+def test_part_2():
+    assert part2() == 112452
