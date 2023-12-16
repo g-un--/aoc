6631492356c3f8d98f7453dd25103047f593c0ec
g-un-- <ginoungureanu.mail@gmail.com>
2023-12-16 09:04:15 +0200

    2023 day 16

--- 2023/day16/day16.py
+++ 2023/day16/day16.py
@@ -55,33 +55,67 @@ def moveBeam(array, startRow, startColumn, startDirection, result, seen):
         
         result.add((row, column))
         seen.add((row, column, direction))
-        
-        match direction:  
-            case "right":
-                column = column + 1
-            case "left":
-                column = column - 1
-            case "up":
-                row = row - 1
-            case "down":
-                row = row + 1
-    
-        if not (0 <= row < rows and 0 <= column < columns):
-            continue
-        
-        nextCellValue = array[row, column]
-    
-        directions = getDirections(nextCellValue, direction)
-        for newDirection in directions:    
-            queue.append((row, column, newDirection))  
+
+        cellValue = array[row, column]
+        directions = getDirections(cellValue, direction)
+        for newDirection in directions:
+            newRow, newColumn = row, column    
+            match newDirection:  
+                case "right":
+                    newColumn = column + 1
+                case "left":
+                    newColumn = column - 1
+                case "up":
+                    newRow = row - 1
+                case "down":
+                    newRow = row + 1
+            
+            if not (0 <= newRow < rows and 0 <= newColumn < columns):
+                continue
+            
+            queue.append((newRow, newColumn, newDirection))  
         
 def part1():
     lines = getLines(__file__)
     array = np.array(list(map(getline, lines)))
     result = set()
     seen = set()
-    moveBeam(array, 0, -1, "right", result, seen)
-    return len(result) - 1
+    moveBeam(array, 0, 0, "right", result, seen)
+    return len(result)
+
+def part2():
+    lines = getLines(__file__)
+    array = np.array(list(map(getline, lines)))
+    rows, columns = np.shape(array)
+    
+    max = 0
+    for row in range(0, rows):
+        result = set()
+        seen = set()
+        moveBeam(array, row, 0, "right", result, seen)
+        result = set()
+        seen = set()
+        if len(result) > max:
+            max = len(result)
+        moveBeam(array, row, columns - 1, "left", result, seen)
+        if len(result) > max:
+            max = len(result)
+            
+    for column in range(0, columns):
+        result = set()
+        seen = set()
+        moveBeam(array, 0, column, "down", result, seen)
+        result = set()
+        seen = set()
+        if len(result) > max:
+            max = len(result)
+        moveBeam(array, rows - 1, column, "up", result, seen)
+        if len(result) > max:
+            max = len(result)    
+    return max
         
 def test_part_1():
-    assert part1() == 46
+    assert part1() == 6605
+    
+def test_part_2():
+    assert part2() == 51
