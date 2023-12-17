a2d313638ffe143f4d2fffea6b606ae726b2c0b3
g-un-- <ginoungureanu.mail@gmail.com>
2023-12-17 19:35:44 +0200

    2023 day 17 part 1

---
+++ 2023/day17/day17.py
@@ -0,0 +1,62 @@
+from utils import getLines
+import numpy as np
+import sys
+import heapq
+
+def getline(line):
+    return [int(c) for c in line]
+
+def getNextDirections(previousDirection):
+    match previousDirection:
+        case "up":
+            return ["up", "left", "right"]
+        case "down":
+            return ["down", "right", "left"]
+        case "left":
+            return ["left", "down", "up"]
+        case "right":
+            return ["right", "up", "down"]
+        
+def getNextPoint(point, direction):
+    row, column = point
+    match direction:
+        case "up":
+            return (row - 1, column)
+        case "down":
+            return (row + 1, column)
+        case "left":
+            return (row, column - 1)
+        case "right":
+            return (row, column + 1)
+        
+def findDistances(array):
+    queue = [(0, 0, 0, "right", 1)]
+    rows, columns = np.shape(array)
+    seen = {}
+    while queue:
+        dist, row, column, direction, steps = heapq.heappop(queue)
+        if (row, column, direction, steps) in seen:
+            continue
+        seen[(row, column, direction, steps)] = dist
+        nextDirections = getNextDirections(direction)
+        for nextDirection in nextDirections:
+            nextRow, nextColumn = getNextPoint((row, column), nextDirection)
+            nextSteps = steps + 1 if nextDirection == direction else 1
+            
+            if 0 <= nextRow < rows and 0 <= nextColumn < columns and nextSteps <= 3:
+                heapq.heappush(queue, (dist + array[nextRow, nextColumn], nextRow, nextColumn, nextDirection, nextSteps))
+    return seen
+        
+def part1():
+    lines = getLines(__file__)
+    array = np.array(list(map(getline, lines)))
+    rows, columns = np.shape(array)
+    result = findDistances(array)
+    min = sys.maxsize
+    for (row, column, _,_), dist in result.items():
+        if row == rows - 1 and column == columns -1 and dist < min:
+            min = dist
+    return min
+        
+def test_part_1():
+    assert part1() == 1039
