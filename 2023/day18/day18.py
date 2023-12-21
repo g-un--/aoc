137ee0ac3d69e3d0db0da474739cd5dcdf10546f
g-un-- <ginoungureanu.mail@gmail.com>
2023-12-21 12:41:41 +0200

    2023 day 18

--- 2023/day18/day18.py
+++ 2023/day18/day18.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def getline(line):
     parts = line.split(' ')
-    return (parts[0], int(parts[1]))
+    return (parts[0], int(parts[1]), int(line[-7:-2], 16), int(line[-2]))
 
 def move(fromPosition, direction, distance):
     row, column = fromPosition
@@ -17,20 +17,27 @@ def move(fromPosition, direction, distance):
         case "D":
             return (row + distance, column)
         
+def getMapForDirection():
+    return {
+        0: "R",
+        1: "D",
+        2: "L",
+        3: "U"
+    }
+        
 def getAreaWithShoelace(path):
-    array = np.array(path)
+    array = np.array(path, dtype='int64')
     x = array[:, 0]
     y = array[:, 1]
     i = np.arange(len(path))
     area = np.abs(np.sum(x[i-1]*y[i]-x[i]*y[i-1])) // 2
     return area
-        
-def part1():
-    lines = list(map(getline, getLines(__file__)))
+
+def solve(moves):
     path = [(0, 0)]
     start = (0, 0)
     perimeter = 0
-    for direction, distance in lines:
+    for direction, distance in moves:
         start = move(start, direction, distance)
         path.append(start)
         perimeter += distance
@@ -38,5 +45,21 @@ def part1():
     i = area - perimeter // 2 + 1
     return i + perimeter
 
+def part1():
+    lines = list(map(getline, getLines(__file__)))
+    moves = list(map(lambda x: (x[0], x[1]), lines))
+    result = solve(moves)
+    return result
+
+def part2():
+    lines = list(map(getline, getLines(__file__)))
+    dirMap = getMapForDirection()
+    moves = list(map(lambda x: (dirMap[x[3]], x[2]), lines))
+    result = solve(moves)
+    return result
+
 def test_part_1():
     assert part1() == 67891
+    
+def test_part_2():
+    assert part2() == 94116351948493
