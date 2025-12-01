b8e51d2ad8584a61d133a6267110143b201fa4d8
Gino Ungureanu <ginoungureanu.mail@gmail.com>
2025-12-01 10:43:21 +0200

    2025 day 1

---
+++ 2025/day1/day1.py
@@ -0,0 +1,35 @@
+from utils import getLines
+
+def part1():
+    lines = getLines(__file__)
+    ops = [(line[0], int(line[1:])) for line in lines]
+    current = 50
+    password = 0
+    for (direction, steps) in ops:
+        current += steps if direction == 'R' else (-steps)
+        current %= 100
+        password += 1 if current == 0 else 0
+    return password
+
+def part2():
+    lines = getLines(__file__)
+    ops = [(line[0], int(line[1:])) for line in lines]
+    current = 50
+    password = 0
+    for (direction, steps) in ops:
+        password += steps // 100
+        steps = steps % 100
+        newCurrent = current + (steps if direction == 'R' else (-steps))
+        if (newCurrent <= 0 and current > 0) or newCurrent >= 100:
+            password += 1
+        current = newCurrent % 100
+    return password
+
+def test_part1():
+    result = part1()
+    assert result == 1147
+
+def test_part2():
+    result = part2()
+    assert result == 6789
+
