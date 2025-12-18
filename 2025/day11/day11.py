5b0abfb69f5bf916e1ddd7fe8a01bb1f8cab1b1a
Gino Ungureanu <ginoungureanu.mail@gmail.com>
2025-12-18 22:44:37 +0200

    2025 day 11 part 2

--- 2025/day11/day11.py
+++ 2025/day11/day11.py
@@ -1,13 +1,14 @@
 from utils import getLines
 from collections import defaultdict
+from functools import cache
 
 
-def dfs(connections, start, stop):
+def dfs(start, stop, connections):
     if start == stop:
         return 1
     result = 0
     for connection in connections[start]:
-        result += dfs(connections, connection, stop)
+        result += dfs(connection, stop, connections)
     return result
 
 
@@ -18,7 +19,32 @@ def part1(input="input.txt"):
         input, rest = line.split(":")
         outputs = rest.split()
         connections[input].extend(outputs)
-    result = dfs(connections, "you", "out")
+    result = dfs("you", "out", connections)
+    return result
+
+
+def part2(input="input.txt"):
+    lines = getLines(__file__, input)
+    connections = defaultdict(list)
+    for line in lines:
+        input, rest = line.split(":")
+        outputs = rest.split()
+        connections[input].extend(outputs)
+
+    @cache
+    def dfs(start, bits):
+        if start == "out":
+            return 1 if bits == 3 else 0
+        if start == "dac":
+            bits |= 2
+        if start == "fft":
+            bits |= 1
+        result = 0
+        for connection in connections[start]:
+            result += dfs(connection, bits)
+        return result
+
+    result = dfs("svr", 0)
     return result
 
 
@@ -30,3 +56,13 @@ def test_part1_example():
 def test_part1():
     result = part1()
     assert result == 428
+
+
+def test_part2_example():
+    result = part2("example2.txt")
+    assert result == 2
+
+
+def test_part2():
+    result = part2()
+    assert result == 331468292364745
